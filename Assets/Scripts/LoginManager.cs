69e98ce1198291a8de0388da631b94522baf8945
Nestor Bermejillo Gonzalez <n.bermejillo.2021@alumnos.urjc.es>
2025-10-21 16:43:47 +0200

    Avances con el login de Google Play

--- Assets/Scripts/LoginManager.cs
+++ Assets/Scripts/LoginManager.cs
@@ -3,10 +3,19 @@ using Unity.Services.Authentication;
 using Unity.Services.Core;
 using UnityEngine;
 
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+
 public class LoginManager : MonoBehaviour
 {
+    private string m_GooglePlayGamesTokem;
+
     private async void Awake()
     {
+        PlayGamesPlatform.DebugLogEnabled = true;
+        PlayGamesPlatform.Activate();
+        LoginGooglePlayGames();
+
        if(UnityServices.State == ServicesInitializationState.Uninitialized)
         {
             Debug.Log("Services Initializing");
@@ -18,6 +27,29 @@ public class LoginManager : MonoBehaviour
     {
         
     }
+    public void LoginGooglePlayGames()
+    {
+        PlayGamesPlatform.Instance.Authenticate((success) =>
+        {
+            if (success == SignInStatus.Success)
+            {
+                Debug.Log("Login with Google Play games successful.");
+
+                PlayGamesPlatform.Instance.RequestServerSideAccess(true, code =>
+                {
+                    Debug.Log("Authorization code: " + code);
+                    m_GooglePlayGamesTokem = code;
+                    // This token serves as an example to be used for SignInWithGooglePlayGames
+                });
+            }
+            else
+            {
+                //Error = "Failed to retrieve Google play games authorization code";
+                Debug.Log($"Google Play Games login unsuccessful");
+            }
+        });
+    }
+
     public async void StartAnonymousSignIn()
     {
         await SignUpAnonymouslyAsync();
@@ -33,6 +65,32 @@ public class LoginManager : MonoBehaviour
             // Shows how to get the playerID
             Debug.Log($"PlayerID: {AuthenticationService.Instance.PlayerId}");
 
+        }
+        catch (AuthenticationException ex)
+        {
+            // Compare error code to AuthenticationErrorCodes
+            // Notify the player with the proper error message
+            Debug.LogException(ex);
+        }
+        catch (RequestFailedException ex)
+        {
+            // Compare error code to CommonErrorCodes
+            // Notify the player with the proper error message
+            Debug.LogException(ex);
+        }
+    }
+
+    private async Task LinkWithUnityAsync(string accessToken)
+    {
+        try
+        {
+            await AuthenticationService.Instance.LinkWithUnityAsync(accessToken);
+            Debug.Log("Link is successful.");
+        }
+        catch (AuthenticationException ex) when (ex.ErrorCode == AuthenticationErrorCodes.AccountAlreadyLinked)
+        {
+            // Prompt the player with an error message.
+            Debug.LogError("This user is already linked with another account. Log in instead.");
         }
         catch (AuthenticationException ex)
         {
