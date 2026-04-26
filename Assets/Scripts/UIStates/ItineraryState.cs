94ddec1f1685699c4b8e189745caf2244a5b8e60
Nestor Bermejillo Gonzalez <n.bermejillo.2021@alumnos.urjc.es>
2026-04-26 13:10:47 +0200

    Ajustes de responsive

--- Assets/Scripts/UIStates/ItineraryState.cs
+++ Assets/Scripts/UIStates/ItineraryState.cs
@@ -5,12 +5,14 @@ public class ItineraryState : UIStateBase
 {
     [SerializeField] private Button backButton;
     [SerializeField] private Button createItineraryButton;
+    [SerializeField] private Button shareButton;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public override void OnEnter()
     {
         ItineraryCrossSceneData.itineraryIdToEdit = null;
         backButton.onClick.AddListener(() => stateManager.ChangeState("Main"));
+        shareButton.onClick.AddListener(() => Sharetinerary());
         createItineraryButton.onClick.AddListener(() => BackgroundTransition.Instance.ToggleTransitionAndLoad("ItineraryScene"));
         //Debug.Log("STATE: Itinerary");
     }
@@ -19,5 +21,10 @@ public class ItineraryState : UIStateBase
     {
         backButton.onClick.RemoveAllListeners();
         createItineraryButton.onClick.RemoveAllListeners();
+    }
+
+    private void Sharetinerary()
+    {
+        
     }
 }
